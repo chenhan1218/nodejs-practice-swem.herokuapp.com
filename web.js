cb72d05a5bf27f320c490595319d062c42c6f0b0
swem <hsnuswem@gmail.com>
2013-03-12 16:48:32 +0800

    test

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
+
+var port = process.env.PORT || 5000;
 r express = require("express");
 var app = express();
 app.get('/', function(req, res) { 
-    res.send("Hello Express Server 2013/3/12 4:36PM");
+    res.send("Hello Express Server 2013/3/12 4:36PM" + process.env.PORT);
     res.end();
 }); 
 
-app.listen(8800);
+var port = process.env.PORT || 5000;
+
+app.listen(port);
